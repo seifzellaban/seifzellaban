2fbb8af5bab39553bba909fc96c6f294df1b510d
Seif Zakaria Mohamed Ellaban <62963968+seifzellaban@users.noreply.github.com>
2023-07-07 01:51:44 +0300

    Update gpA.cpp

--- gpA.cpp
+++ gpA.cpp
@@ -11,21 +11,20 @@ int main() {
     int numCourses = 0;
     double totalCredits = 0.0, cumulativeCredits = 0.0;
     double totalGradePoints = 0.0, cumulativeGpa = 0.0;
-    
+    char addAnother = 'y';
     cout << "GRADE CALCULATOR ON A 4 POINT GPA SYSTEM." << endl << endl;
-    
+
     // Get cumulative gpa
     cout << "Do you have a cumulative GPA? (y/n): ";
     cin >> addAnother;
-    if (addAnother == 'y')
+    if (addAnother == 'y' || addAnother == 'Y')
     {
-        cout << "Enter cumulative GPA: " << endl;
+        cout << "Enter cumulative GPA: ";
         cin >> cumulativeGpa;
-        cout >> "Enter cumulative credit hours";
+        cout << "Enter cumulative credit hours: ";
         cin >> cumulativeCredits;
     }
     // Get course information from the user
-    char addAnother = 'y';
     while (numCourses < maxCourses && (addAnother == 'y' || addAnother == 'Y')) {
         cout << "Enter course name: ";
         cin >> courseNames[numCourses];
@@ -33,58 +32,70 @@ int main() {
         cin >> credit[numCourses];
         cout << "Enter grade (A, A-, B+, B, B-, C+, C, C-, D, F): ";
         cin >> gradeString[numCourses];
-        
+
         // Convert grade to numeric value
         double grade = 0.0;
         if (gradeString[numCourses] == "A") {
             grade = 4.0;
-        } else if (gradeString[numCourses] == "A-") {
+        }
+        else if (gradeString[numCourses] == "A-") {
             grade = 3.67;
-        } else if (gradeString[numCourses] == "B+") {
+        }
+        else if (gradeString[numCourses] == "B+") {
             grade = 3.33;
-        } else if (gradeString[numCourses] == "B") {
+        }
+        else if (gradeString[numCourses] == "B") {
             grade = 3.0;
-        } else if (gradeString[numCourses] == "B-") {
+        }
+        else if (gradeString[numCourses] == "B-") {
             grade = 2.67;
-        } else if (gradeString[numCourses] == "C+") {
+        }
+        else if (gradeString[numCourses] == "C+") {
             grade = 2.33;
-        } else if (gradeString[numCourses] == "C") {
+        }
+        else if (gradeString[numCourses] == "C") {
             grade = 2.0;
-        } else if (gradeString[numCourses] == "C-") {
+        }
+        else if (gradeString[numCourses] == "C-") {
             grade = 1.67;
-        } else if (gradeString[numCourses] == "D") {
+        }
+        else if (gradeString[numCourses] == "D") {
             grade = 1.0;
-        } else if (gradeString[numCourses] == "F") {
+        }
+        else if (gradeString[numCourses] == "F") {
             grade = 0.0;
-        } else {
+        }
+        else {
             cout << "Invalid grade entered. Please try again." << endl;
             continue;
         }
-        
+
         // Update GPA calculation variables
         totalCredits += credit[numCourses];
         totalGradePoints += grade * credit[numCourses];
         numCourses++;
-        
+
         // Ask if user wants to add another course
         cout << "Add another course? (y/n): ";
         cin >> addAnother;
     }
-    
+
     // Calculate and display GPA
     if (numCourses == 0) {
         cout << "No courses entered. GPA cannot be calculated." << endl;
-    } else {
-        if (cumulativeCredits == 0)
+    }
+    else {
+        double gpa = 0.0;
+        if (cumulativeCredits != 0)
         {
-            double gpa = (totalGradePoints + (cumulativeGpa * cumulativeCredits)) / (totalCredits + cumulativeCredits);
+            gpa = (totalGradePoints + (cumulativeGpa * cumulativeCredits)) / (totalCredits + cumulativeCredits);
         }
         else
         {
-            double gpa = (totalGradePoints / totalCredits);
+            gpa = (totalGradePoints / totalCredits);
         }
         cout << endl << "GPA: " << gpa << endl;
     }
-    
+
     return 0;
 }
