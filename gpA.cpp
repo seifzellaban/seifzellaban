cd32a2901fb9ef05a6e981f48532f6a0886595fc
Seif Zakaria Mohamed Ellaban <62963968+seifzellaban@users.noreply.github.com>
2023-07-07 01:37:27 +0300

    Update gpA.cpp

--- gpA.cpp
+++ gpA.cpp
@@ -1,108 +1,90 @@
 #include <iostream>
-#include <iomanip>
+#include <string>
+
 using namespace std;
 
-string error() 
-{
-    string errorList = "\nList of Possible Errors:\n(0x0001): Unacceptable value inputted.\n";
-    return errorList;
-}
-
-int main()
-{
-    //Declaration.
-    float gpa = 4, gpaM = 0, cumulativeGPA, creditSum = 0, credit[1000], grade[1000];
-    string courseNames[1000], gradeString[1000];
-    int n = -1;
-    string selector;
-
-    cout << "GRADE CALCULATOR ON A 4 POINT GPA SYSTEM.\n";
-    cout << "Do You Want to Calculate Cumulative GPA With GPAs From Past Semesters? (y for Yes, n for No): ";
-    cin >> selector;
-
-    //Cumulative GPA Prompt.
-    if (selector == "y" || selector == "Y")
+int main() {
+    const int maxCourses = 100;
+    string courseNames[maxCourses];
+    int credit[maxCourses];
+    string gradeString[maxCourses];
+    int numCourses = 0;
+    double totalCredits = 0.0, cumulativeCredits = 0.0;
+    double totalGradePoints = 0.0, cumulativeGpa = 0.0;
+    
+    cout << "GRADE CALCULATOR ON A 4 POINT GPA SYSTEM." << endl << endl;
+    
+    // Get cumulative gpa
+    cout << "Do you have a cumulative GPA? (y/n): ";
+    cin >> addAnother;
+    if (addAnother == 'y')
     {
-        cout << "Is the old GPA with you? (y for Yes, n for No): ";
-        cin >> selector;
-            //Old GPA Collection.
-            if (selector == "y" || selector == "Y")
-            {
-                selector = "0";
-                cout << "Enter the old cumulative GPA: ";
-                cin >> cumulativeGPA;
-
-            }
-            else if (selector == "n" || selector == "N")
-            {
-                selector = "0";
-                cout << "No worries, we will calculate it for you. \n";
-            }
-            else
-            {
-                cout << "\nRun Time Error (0x0001)" << error();
-                return 0;
-            }
+        cout << "Enter cumulative GPA: " << endl;
+        cin >> cumulativeGpa;
+        cout >> "Enter cumulative credit hours";
+        cin >> cumulativeCredits;
     }
-    else if (selector == "n" || selector == "N")
-    {
-        selector = "skip";
-        cout << "Alright we will just process this semester's grade. \n";
+    // Get course information from the user
+    char addAnother = 'y';
+    while (numCourses < maxCourses && (addAnother == 'y' || addAnother == 'Y')) {
+        cout << "Enter course name: ";
+        cin >> courseNames[numCourses];
+        cout << "Enter number of credit hours: ";
+        cin >> credit[numCourses];
+        cout << "Enter grade (A, A-, B+, B, B-, C+, C, C-, D, F): ";
+        cin >> gradeString[numCourses];
+        
+        // Convert grade to numeric value
+        double grade = 0.0;
+        if (gradeString[numCourses] == "A") {
+            grade = 4.0;
+        } else if (gradeString[numCourses] == "A-") {
+            grade = 3.67;
+        } else if (gradeString[numCourses] == "B+") {
+            grade = 3.33;
+        } else if (gradeString[numCourses] == "B") {
+            grade = 3.0;
+        } else if (gradeString[numCourses] == "B-") {
+            grade = 2.67;
+        } else if (gradeString[numCourses] == "C+") {
+            grade = 2.33;
+        } else if (gradeString[numCourses] == "C") {
+            grade = 2.0;
+        } else if (gradeString[numCourses] == "C-") {
+            grade = 1.67;
+        } else if (gradeString[numCourses] == "D") {
+            grade = 1.0;
+        } else if (gradeString[numCourses] == "F") {
+            grade = 0.0;
+        } else {
+            cout << "Invalid grade entered. Please try again." << endl;
+            continue;
+        }
+        
+        // Update GPA calculation variables
+        totalCredits += credit[numCourses];
+        totalGradePoints += grade * credit[numCourses];
+        numCourses++;
+        
+        // Ask if user wants to add another course
+        cout << "Add another course? (y/n): ";
+        cin >> addAnother;
     }
-    else 
-    {
-        cout << "\nRun Time Error (0x0001)" << error();
-        return 0;
-    }
-
-    do
-    {
-        n++;
-        cout << "Enter Course Name: ";
-        cin >> courseNames[n];
-        cout << "Enter number course credit hours: ";
-        cin >> credit[n];
-        cout << "Enter grade Only(A, A-, B+, B, B-, C+, C, C-, D, F) are accepted: ";
-        cin >> gradeString[n];
-        selector = "skip";
-        cout << "Total Number of Courses Registered is " << n + 1 << " Courses, Do You Want to Add More? (y for Yes, n for No): " << std::endl;
-        cin >> selector;
-    } while (selector != "n" || selector != "N");
-
-    for (int i = 0; i < n; i++)
-    {
-        if (gradeString[i] == "A")
-            grade[i] = 4;
-        else if (gradeString[i] == "A-")
-            grade[i] = 3.67;
-        else if (gradeString[i] == "B+")
-            grade[i] = 3.33;
-        else if (gradeString[i] == "B+")
-            grade[i] = 3;
-        else if (gradeString[i] == "B-")
-            grade[i] = 2.67;
-        else if (gradeString[i] == "C+")
-            grade[i] = 2.33;
-        else if (gradeString[i] == "C")
-            grade[i] = 2;
-        else if (gradeString[i] == "C-")
-            grade[i] = 1.67;
-        else if (gradeString[i] == "D")
-            grade[i] = 1;
+    
+    // Calculate and display GPA
+    if (numCourses == 0) {
+        cout << "No courses entered. GPA cannot be calculated." << endl;
+    } else {
+        if (cumulativeCredits == 0)
+        {
+            double gpa = (totalGradePoints + (cumulativeGpa * cumulativeCredits)) / (totalCredits + cumulativeCredits);
+        }
         else
-            grade[i] = 0;
+        {
+            double gpa = (totalGradePoints / totalCredits);
+        }
+        cout << endl << "GPA: " << gpa << endl;
     }
-
-    for (int j = 0; j < (n+1); j++)
-    {
-        creditSum += credit[j];
-    }
-    for (int k = 0; k < n; k++)
-    {
-        gpaM += credit[k] * grade[k];
-    }
-    gpa = gpaM / creditSum;
-    cout << "Your GPA is: " << gpa << " of 4.\n";
-    system("pause");
+    
     return 0;
 }
